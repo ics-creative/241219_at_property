6208d0a8c0cd3d00ce12b7a4cf6ed831e62793ae
ics-nishihara <nishihara@ics-web.jp>
2024-12-18 16:36:08 +0900

    デモの作成

---
+++ src/assets/style/style.js
@@ -0,0 +1,8 @@
+import "./common.css";
+import "./index.css";
+import "./base.css";
+import "./circular.css";
+import "./gamingText.css";
+import "./spiral.css";
+import "./scaling.css";
+import "./ball.css";
